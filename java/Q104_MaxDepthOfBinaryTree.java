897286072cd798299bef29b06c647fe21d2afb22
Yatong Jiang <yatong.jiang@outlook.com>
2022-09-16 12:26:38 +1000

    Create Q104_MaxDepthOfBinaryTree.java

---
+++ java/Q104_MaxDepthOfBinaryTree.java
@@ -0,0 +1,39 @@
+package java;
+
+
+/** Tree & Depth-first-search
+ * Given the root of a binary tree, return its maximum depth.
+ * A binary tree's maximum depth is the number of nodes along the longest path from 
+ * the root node down to the farthest leaf node.
+ * 
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/* Test:
+ * 1. tree with no node
+ * 2. tree with only one node
+ * 3. inbalanced tree
+ * 4. large tree
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return 1 + Math.max(left, right);
+    }
+}
+    
